65d5c49cfe9cf494e5acfe480cd196ad3e755701
PlGGS <bbor0422@gmail.com>
2023-03-12 23:22:25 -0500

    Added python script Updated README Added gitignore

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # spotify-liked-songs-to-playlist
+This script allows you to copy your Liked Songs on Spotify to an actual playlist.
+
+This solves an issue where Spotify won't play your Liked Songs on devices other than mobile.
+
+Before running, make sure to do the following:
+
+1. Install spotipy using pip
+2. Create a new playlist or choose an existing one and copy its id from its  url in your browser
+3. Set up a Spotify app at: https://developer.spotify.com
+4. Export your client id - Example: export SPOTIPY_CLIENT_ID=[your id goes here]
+5. Export your client secret - Example: export SPOTIPY_CLIENT_SECRET=[your secret goes here]
+6. Export your redirect uri - Example: export SPOTIPY_REDIRECT_URI=http://localhost:8888
+
+Enjoy!
---
+++ spot.py
@@ -0,0 +1,23 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# READ THE README AND THEN SET THIS BEFORE RUNNING
+playlist_id = ''
+
+scope = "user-library-read playlist-modify-public"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+print('getting tracks. This will take a bit...')
+# https://stackoverflow.com/questions/39086287/spotipy-how-to-read-more-than-100-tracks-from-a-playlist
+def get_liked_songs():
+    results = sp.current_user_saved_tracks()
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+    return tracks
+
+for idx, item in enumerate(get_liked_songs()):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
+    sp.playlist_add_items(playlist_id, [track['uri']])  
