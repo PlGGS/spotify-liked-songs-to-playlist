3ee8fd9658f4e5b27830bfbcb84933a5d9c4a929
PlGGS <bbor0422@gmail.com>
2023-03-14 23:07:12 -0500

    Added command line param option for playlist id

--- spot.py
+++ spot.py
@@ -1,13 +1,21 @@
+import sys
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 # READ THE README AND THEN SET THIS BEFORE RUNNING
 playlist_id = ''
 
+if len(sys.argv) == 1:
+    if playlist_id == '':
+        print('Please assign a playlist id either within the script or as a command line parameter')
+        sys.exit()
+else:
+    playlist_id = sys.argv[1]
+
 scope = "user-library-read playlist-modify-public"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-print('getting tracks. This will take a bit...')
+print('Getting tracks. This will take a bit...')
 # https://stackoverflow.com/questions/39086287/spotipy-how-to-read-more-than-100-tracks-from-a-playlist
 def get_liked_songs():
     results = sp.current_user_saved_tracks()
